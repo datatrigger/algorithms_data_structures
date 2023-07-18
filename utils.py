c0c480cc168d00dc7508e0cb38ec2229276b6344
Vincent Le Goualher <v.legoualher@gmail.com>
2023-07-18 08:10:52 +0200

    Add utils for sorting algos - linked lists - trees

---
+++ utils.py
@@ -0,0 +1,107 @@
+from __future__ import annotations
+from typing import Callable
+from dataclasses import dataclass, field
+from collections import deque
+
+# Sorting
+def validate_sorting(input_algo: Callable) -> bool:
+    # Make sure `input_algo` returns the sorted list
+    if input_algo([0]) is None:
+        def algo(l: list[int]) -> list[int]:
+            input_algo(l)
+            return l
+    else:
+        algo = input_algo
+
+    assert algo([]) == [], "Error: Empty list"
+    assert algo([1]) == [1], "Error: One-element list"
+    assert algo([1, 1, 1]) == [1, 1, 1], "Error: List of duplicates."
+    assert algo([1, 2, 3]) == [1, 2, 3], "Error: Sorted list"
+    assert algo([1, 1, 1, 2, 2, 2, 2, 3]) == [1, 1, 1, 2, 2, 2, 2, 3], "Error: Sorted list with duplicates"
+    assert algo([7, 2, 5, 6, 1, 9]) == [1, 2, 5, 6, 7, 9], "Error: Unsorted list"
+    assert algo([7, 2, 1, 6, 1, 9, 5, 2]) == [1, 1, 2, 2, 5, 6, 7, 9], "Error: Unsorted list with duplicates"
+    
+    print(f"{input_algo.__name__} successfully sorted all test arrays!")
+    return True
+
+# Linked lists
+@dataclass
+class ListNode:
+    val: int|None
+    next: int = None
+
+    def __str__(self: ListNode) -> str:
+        output = "-> "
+        curr = self
+        while curr:
+            output += f"{curr.val} -> "
+            curr = curr.next
+        return output
+
+def create_llist(vals: list[int]|None) -> ListNode:
+    dummy = ListNode(None)
+    curr = dummy
+    for val in vals:
+        curr.next = ListNode(val)
+        curr = curr.next
+    return dummy.next
+
+def check_equal_llist(head1: ListNode|None, head2:ListNode|None) -> bool:
+    curr1, curr2 = head1, head2
+    while curr1 and curr2:
+        if curr1.val != curr2.val:
+            return False
+        curr1, curr2 = curr1.next, curr2.next
+    return not curr1 and not curr2
+
+def validate_reversing(algo: Callable) -> bool:
+    assert check_equal_llist(algo(create_llist([])), create_llist([])), "Error: Empty list"
+    assert check_equal_llist(algo(create_llist([1, 2])), create_llist([2, 1])), "Error: basic list"
+    assert check_equal_llist(algo(create_llist([1, 2, 3])), create_llist([3, 2, 1])), "Error: basic list"
+
+    print(f"{algo.__name__} successfully reversed all test linked lists!")
+    
+# Binary trees
+@dataclass
+class TreeNode:
+    val: int
+    left: TreeNode = None
+    right: TreeNode = None
+
+def tree_to_list(root: TreeNode) -> list[int|None]:
+    values = []
+    still_has_nodes = True
+    queue = deque()
+    queue.append(root)
+
+    while still_has_nodes:
+        still_has_nodes = False
+        for _ in range(len(queue)):
+            curr = queue.popleft()
+            if curr:
+                if curr.left or curr.right:
+                    still_has_nodes = True
+                queue.append(curr.left)
+                queue.append(curr.right)
+                values.append(curr.val)
+            else:
+                queue.append(None)
+                queue.append(None)
+                values.append(None)
+    while len(values) >= 2 and values[-1] is None:
+        values.pop()
+    return values
+    
+root = TreeNode(1, None, TreeNode(3, None, TreeNode(5)))
+print(tree_to_list(root))
+
+def list_to_tree(values: list[int|None]) -> TreeNode:
+    nodes = [None if value is None else TreeNode(value) for value in values]
+    for i, node in enumerate(nodes):
+        if node:
+            left_idx, right_idx = 2 * i + 1, 2 * i + 2
+            if left_idx < len(nodes):
+                node.left = nodes[left_idx]
+            if right_idx < len(nodes):
+                node.right = nodes[right_idx]
+    return nodes[0]

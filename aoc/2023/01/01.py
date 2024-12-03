4bdb16ff30a6a77f9ff845b6cbe610092da69910
Vincent Le Goualher <vlg.engineer@gmail.com>
2024-12-03 17:23:03 +0000

    Write aoc 2023 day 1

--- aoc/2023/01/01.py
+++ aoc/2023/01/01.py
@@ -1,10 +1,9 @@
 with open("input.txt", "r") as f:
 #with open("sample.txt", "r") as f:
-    lines = (line.strip() for line in f.readlines())
+    lines = [line.strip() for line in f.readlines()]
 
 # 1
 def get_number(line):
-    first = last = "0"
     for c in line:
         if c.isdigit():
             first = c
@@ -19,4 +18,38 @@ res1 = sum(get_number(line) for line in lines)
 print(res1)
 
 # 2
+nums = {
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+}
 
+def get_number2(line):
+    first = last = ""
+    i = 0
+    while i < len(line) and not first:
+        if line[i].isdigit():
+            first = line[i]
+        for key, val in nums.items():
+            if line[i:i + len(key)] == key:
+                first = val
+        i += 1
+    i = len(line) - 1
+    while i >= 0 and not last:
+        if line[i].isdigit():
+            last = line[i]
+        for key, val in nums.items():
+            if line[i:i + len(key)] == key:
+                last = val
+        i -= 1
+    return int(first + last)
+
+res2 = sum(get_number2(line) for line in lines)
+print(res2)

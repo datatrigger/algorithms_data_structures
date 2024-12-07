e1d4d836140ad3b525a8cd8a73061842080bb203
Vincent Le Goualher <vlg.engineer@gmail.com>
2024-12-07 06:04:57 +0000

    Write AOC 7

--- aoc/2024/07/07.py
+++ aoc/2024/07/07.py
@@ -1,8 +1,28 @@
-from copy import deepcopy
-
-with open("sample.txt", "r") as f:
-    lines = (line.strip() for line in f.readlines())
+with open("input.txt", "r") as f:
+    lines = [line.strip() for line in f.readlines()]
 
 #1
+def is_valid(line, part=1):
+    val, nums = line.split(":")
+    val, nums = int(val), [int(num) for num in nums.strip().split(" ")]
+
+    cache = {}
+    def f(i, curr):
+        if i == len(nums):
+            return curr == val
+        if (i, curr) not in cache:
+            add = f(i + 1, curr + nums[i])
+            multiply = f(i + 1, curr * nums[i])
+            cache[(i, curr)] = add or multiply
+            if part == 2:
+                concat = f(i + 1, int(str(curr) + str(nums[i])))
+                cache[(i, curr)] = cache[(i, curr)] or concat
+        return cache[(i, curr)]
+    return val if f(0, 0) else 0
+
+res1 = sum(is_valid(line, part=1) for line in lines)
+print(res1)
 
 #2
+res2 = sum(is_valid(line, part=2) for line in lines)
+print(res2)

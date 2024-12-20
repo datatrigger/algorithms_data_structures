8445c04815309a3e26d67394641b8416d9c3c8ca
Vincent Le Goualher <vincent.legoualher@elca.ch>
2024-12-20 16:02:56 +0100

    Write day 16, still need to refactor

--- aoc/2024/16/16.py
+++ aoc/2024/16/16.py
@@ -1,69 +1,86 @@
+import sys
+sys.setrecursionlimit(10000)
 import heapq
+from time import sleep
 
-input_file = "sample"
+input_file = "input"
 with open(f"{input_file}.txt", "r") as f:
     m = [line.strip() for line in f.readlines()]
 
-directions = {
-    "^": (-1, 0),
-    ">": (0, 1),
-    "v": (1, 0),
-    "<": (0, -1)
-}
-
 #1
-
 R, C = len(m), len(m[0])
 for r in range(R):
     for c in range(C):
         if m[r][c] == "S":
-            start = (r, c, 0, 1)
-            rstart, cstart = r, c
+            start = ((r, c), (0, 1))
         if m[r][c] == "E":
             rend, cend = r, c
 
 def get_shortest_path_distance():
-    heap = [(0, start)]
+    heap = [(0, *start)]
     visited = set()
     while heap:
-        d, (r, c, curr_dr, curr_dc) = heapq.heappop(heap)
-        if (r, c, curr_dr, curr_dc) in visited:
+        d, pos, direction = heapq.heappop(heap)
+        if (pos, direction) in visited:
             continue
-        visited.add((r, c, curr_dr, curr_dc))
-        if r == rend and c == cend:
+        visited.add((pos, direction))
+
+        if pos[0] == rend and pos[1] == cend:
             return d
         for dr, dc in ((-1, 0), (0, 1), (1, 0), (0, -1)):
-            if m[r + dr][c + dc] == "#":
+            if m[pos[0] + dr][pos[1] + dc] == "#":
                 continue
-            if dr == curr_dr and dc == curr_dc:
-                heapq.heappush(heap, (d + 1, (r + dr, c + dc, dr, dc)))
+            if dr == direction[0] and dc == direction[1]:
+                heapq.heappush(heap, (d + 1, (pos[0] + dr, pos[1] + dc), (dr, dc)))
             else:
-                heapq.heappush(heap, (d + 1000, (r, c, dr, dc)))
-    return v
+                heapq.heappush(heap, (d + 1000, (pos[0], pos[1]), (dr, dc)))
+
 shortest_distance = get_shortest_path_distance()
 print(shortest_distance)
 
 #2
-parents = {}
-heap = [(0, start, start)] #cost, node, parent
-tiles = set([(rstart, cstart), (rend, cend)])
+from collections import defaultdict
+
+heap = [(0, *start, (-1, -1), (-1, -1))]
+shortest = {}
+parents = defaultdict(set)
 while heap:
-    d, (r, c, curr_dr, curr_dc), parent = heapq.heappop(heap)
-    if (r, c, curr_dr, curr_dc) in parents:
+    d, pos, direction, parent_pos, parent_direction = heapq.heappop(heap)
+    if d > shortest_distance:
+        break
+    if (pos, direction) in shortest:
+        if shortest[(pos, direction)] == d:
+            parents[(pos, direction)].add((parent_pos, parent_direction))
         continue
-    parents[(r, c, curr_dr, curr_dc)] = parent
-    if r == rend and c == cend and d == shortest_distance:
-        print(f"Found {r, c, curr_dr, curr_dc}")
-        pr, pc, pdr, pdc = parent
-        while not(pr == rstart and pc == cstart and pdr == 0 and pdc == 1):
-            tiles.add((pr, pc))
-            pr, pc, pdr, pdc = parents[(pr, pc, pdr, pdc)]
+    shortest[(pos, direction)] = d
+    parents[(pos, direction)].add((parent_pos, parent_direction))
     for dr, dc in ((-1, 0), (0, 1), (1, 0), (0, -1)):
-        if m[r + dr][c + dc] == "#":
+        if m[pos[0] + dr][pos[1] + dc] == "#":
             continue
-        if dr == curr_dr and dc == curr_dc:
-            heapq.heappush(heap, (d + 1, (r + dr, c + dc, dr, dc), (r, c, curr_dr, curr_dc)))
+        if dr == direction[0] and dc == direction[1]:
+            heapq.heappush(heap, (
+                d + 1,
+                (pos[0] + dr, pos[1] + dc),
+                (dr, dc),
+                pos,
+                direction
+                )
+            )
         else:
-            heapq.heappush(heap, (d + 1000, (r, c, dr, dc), (r, c, curr_dr, curr_dc)))
+            heapq.heappush(heap, (d + 1000, (pos[0], pos[1]), (dr, dc), pos, direction))
+
+end_nodes = [(r, c, dr, dc) for ((r, c), (dr, dc)) in shortest if shortest[((r, c), (dr, dc))] == shortest_distance and r == rend and c == cend]
+
+rstart, cstart = start[0]
+tiles = set([(rstart, cstart)])
+def dfs(r, c, dr, dc):
+    if r == rstart and c == cstart:
+        return
+    tiles.add((r, c))
+    for ((pr, pc), (pdr, pdc)) in parents[((r, c), (dr, dc))]:
+        dfs(pr, pc, pdr, pdc)
+
+for end_node in end_nodes:
+    dfs(*end_node)
 
 print(len(tiles))

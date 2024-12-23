07ff663d59d3b29ddfbad39351e62ec890003749
Vincent Le Goualher <vincent.legoualher@elca.ch>
2024-12-23 15:22:45 +0100

    Write AOC day 19

---
+++ aoc/2024/19/19.py
@@ -0,0 +1,37 @@
+# Input
+input_file = "input"
+with open(f"{input_file}.txt", "r") as f:
+    lines = [line.strip() for line in f.readlines()]
+
+patterns = lines[0].split(", ")
+designs = lines[2:]
+
+# 1
+cache = {}
+def is_possible(design: str):
+    if design == "":
+        return True
+    if design not in cache:
+        cache[design] = False
+        for pattern in patterns:
+            if design.startswith(pattern):
+                cache[design] = cache[design] or is_possible(design[len(pattern):])
+    return cache[design]
+
+count = sum(1 for design in designs if is_possible(design))
+print(count)
+
+# 1
+cache = {}
+def ways_to_make(design: str):
+    if design == "":
+        return 1
+    if design not in cache:
+        cache[design] = 0
+        for pattern in patterns:
+            if design.startswith(pattern):
+                cache[design] += ways_to_make(design[len(pattern):])
+    return cache[design]
+
+ways = sum(ways_to_make(design) for design in designs)
+print(ways)
